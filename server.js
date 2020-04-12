ff5f9bf338501e981bee4530c13335e1b00448d7
Glitch (cover-hack) <none>
2020-04-12 22:12:15 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+// init project
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + '/app/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
