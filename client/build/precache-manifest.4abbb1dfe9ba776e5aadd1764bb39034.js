5135eeb8baabdef55bb5e3782341c3ead6ed87df
Glitch (cover-hack) <none>
2020-05-12 22:06:14 +0000

    Pnpm bug

---
+++ client/build/precache-manifest.4abbb1dfe9ba776e5aadd1764bb39034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8b79f086bbde0e44f101c57807bbff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4be350b107d5044e21",
+    "url": "/static/css/main.a27c12bc.chunk.css"
+  },
+  {
+    "revision": "7c362fef71bfe4f8f17e",
+    "url": "/static/js/2.de96649c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.de96649c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4be350b107d5044e21",
+    "url": "/static/js/main.adbb9eb6.chunk.js"
+  },
+  {
+    "revision": "fdd7f42969f6beeb6996",
+    "url": "/static/js/runtime-main.e50b6a85.js"
+  }
+]);
